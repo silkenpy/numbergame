1e309f90dce620ea4499d7b5e155c21a9a4e2657
a.a <silkenpy@gmail.com>
2021-02-13 17:08:13 +0330

    Move class to package

--- app.py
+++ app.py
@@ -1,23 +1,7 @@
 import falcon
 
-
-# Falcon follows the REST architectural style, meaning (among
-# other things) that you think in terms of resources and state
-# transitions, which map to HTTP verbs.
-# class ThingsResource(object):
-#     def on_post(self, req, resp):
-#         """Handles GET requests"""
-#         resp.status = falcon.HTTP_200  # This is the default status
-#         resp.content_type = falcon.MEDIA_HTML
-#         resp.body = ('<!DOCTYPE html><html><body><h1>My First Heading</h1><p>My first paragraph.</p></body></html>')
-
-# falcon.API instances are callable WSGI apps
 from numbergame.api.v1.hello import Hello
 
 app = falcon.API()
 
-# Resources are represented by long-lived class instances
-# things = ThingsResource()
-
-# things will handle all requests to the '/things' URL path
 app.add_route('/', Hello())
--- numbergame/api/v1/hello.py
+++ numbergame/api/v1/hello.py
@@ -2,8 +2,8 @@ import falcon
 
 
 class Hello(object):
-    def on_post(self, req, resp):
+    def on_get(self, req, resp):
         """Handles GET requests"""
         resp.status = falcon.HTTP_200  # This is the default status
         resp.content_type = falcon.MEDIA_HTML
-        resp.body = ('<!DOCTYPE html><html><body><h1>My First Heading</h1><p>My first paragraph.</p></body></html>')
+        resp.body = ('<!DOCTYPE html><html><body><h1>AdadBazi</h1><p>Welcome to AdadBazi</p></body></html>')
