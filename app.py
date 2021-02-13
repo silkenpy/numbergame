bdb305f546afef73742dd0234fce4150255b96b5
a.a <silkenpy@gmail.com>
2021-02-13 16:59:32 +0330

    Add first app

---
+++ app.py
@@ -0,0 +1,21 @@
+import falcon
+
+
+# Falcon follows the REST architectural style, meaning (among
+# other things) that you think in terms of resources and state
+# transitions, which map to HTTP verbs.
+class ThingsResource(object):
+    def on_post(self, req, resp):
+        """Handles GET requests"""
+        resp.status = falcon.HTTP_200  # This is the default status
+        resp.content_type = falcon.MEDIA_HTML
+        resp.body = ('<!DOCTYPE html><html><body><h1>My First Heading</h1><p>My first paragraph.</p></body></html>')
+
+# falcon.API instances are callable WSGI apps
+app = falcon.API()
+
+# Resources are represented by long-lived class instances
+things = ThingsResource()
+
+# things will handle all requests to the '/things' URL path
+app.add_route('/', things)
