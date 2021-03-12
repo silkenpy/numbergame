3f33982c8a51120c8491fc35ecff08a3ae87ed72
a.a <silkenpy@gmail.com>
2021-03-12 12:18:43 +0330

    Add body, params, headers to response for checking

--- numbergame/api/v1/version.py
+++ numbergame/api/v1/version.py
@@ -1,4 +1,5 @@
 """Hello page."""
+import json
 from datetime import datetime
 
 import falcon
@@ -10,7 +11,36 @@ from numbergame.settings import VERSION
 class Version:
     """Simple html page."""
 
-    def on_get(self, _: Request, resp: Response) -> None:
+    def on_get(self, req: Request, resp: Response) -> None:
         """GET /v1/version simple page."""
         resp.status = falcon.HTTP_200
-        resp.media = {"version": VERSION, "date": str(datetime.now())}
+        raw_body = req.bounded_stream.read()
+        body = ""
+        if raw_body:
+            body = json.loads(raw_body)
+
+        resp.media = {
+            "Method": "GET",
+            "Params": req.params,
+            "Headers": req.headers,
+            "Body": body,
+            "version": VERSION,
+            "date": str(datetime.now())
+        }
+
+    def on_post(self, req: Request, resp: Response) -> None:
+        """POST /v1/version simple page."""
+        resp.status = falcon.HTTP_200
+        raw_body = req.bounded_stream.read()
+        body = ""
+        if raw_body:
+            body = json.loads(raw_body)
+
+        resp.media = {
+            "Method": "POST",
+            "Params": req.params,
+            "Headers": req.headers,
+            "Body": body,
+            "version": VERSION,
+            "date": str(datetime.now())
+        }
